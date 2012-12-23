4c3df5a6881048bf9b4ccb88b7f4153330f1a3d5
Barnaby Walters <barnaby@waterpigs.co.uk>
2012-12-23 12:20:45 +0000

    Added template expansion function, started writing content scripts, added OYC embed height to options

--- src/common/content_script.js
+++ src/common/content_script.js
@@ -8,6 +8,35 @@
 
 var OwnYourComments = (function () {
 	// Private
+	// Utility functions
+	function getConfig(key, callback) {
+		kango.invokeAsync('kango.storage.getItem', key, callback);
+	}
+	
+	/**
+	 * Expands {name} templates with the replace.name, or the empty string
+	 */
+	function expandTemplates(postURL, replace) {
+		for (var template in replace) {
+        	if (replace[template] == undefined)
+            	continue;
+            postURL = postURL.split('{' + template + '}')
+            	.join(encodeURIComponent(replace[template].replace(/^\s+|\s+$/g, '')));
+		}
+		
+		// replace any unreplaced templates with nothing
+		postURL = postURL.replace(/\{[a-zA-Z0-9_-]+\}/gi, '');
+		
+		return postURL;
+	}
+	
+	// Content manipuation functions
+	/**
+	 * Embeds an Indieweb comments box before Disqus or Livefyre iframes
+	 */
+	function embedCommentsBox(url) {
+		$()
+	}
 	
 	// Public
 	return {
