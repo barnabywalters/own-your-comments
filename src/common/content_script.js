8bfd4e3f36f59b38477ab899558436e6996866a8
Barnaby Walters <barnaby@waterpigs.co.uk>
2012-12-23 12:34:51 +0000

    Corrected require jquery

--- src/common/content_script.js
+++ src/common/content_script.js
@@ -3,7 +3,7 @@
 // @include http://*
 // @include https://*
 // @require URI.js
-// @require jquery-1.8.3-min.js
+// @require jquery-1.8.3.min.js
 // ==/UserScript==
 
 var OwnYourComments = (function () {
